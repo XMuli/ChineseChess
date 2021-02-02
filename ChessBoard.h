43052b0804b135ec0df1389edadd5dba341bcc9b
xmuli <xmulitech@gmail.com>
2021-02-02 18:05:20 +0800

    feat: add Adding connection IP functionality（UI）

--- ChessBoard.h
+++ ChessBoard.h
@@ -119,9 +119,10 @@ private slots:
 
     void on_pushButton_restart_clicked();
 
-private:
+public:
     Ui::ChessBoard *ui;
 
+private:
     QTimer * m_timer;      //定时器 每秒更新时间
     QTime * m_timeRecord;  //记录时间
     bool m_bIsStart;        //记录是否已经开始计时
