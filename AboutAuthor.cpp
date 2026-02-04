1b94ff587415fed79fb85921212f8860aa26670f
XMuli <xmulitech@gmail.com>
2026-02-04 17:41:42 +0800

    refactor： aupdate About Page

--- AboutAuthor.cpp
+++ AboutAuthor.cpp
@@ -7,7 +7,6 @@
 #include "ui_AboutAuthor.h"
 #include <QIcon>
 #include <QPainter>
-#include <QPalette>
 
 AboutAuthor::AboutAuthor(QWidget *parent) :
     QDialog(parent),
@@ -15,10 +14,6 @@ AboutAuthor::AboutAuthor(QWidget *parent) :
 {
     this->setWindowIcon(QIcon(":/images/chess.svg"));
     ui->setupUi(this);
-
-    QPalette pl = ui->textBrowser->palette();
-    pl.setBrush(QPalette::Base,QBrush(QColor(255,0,0,0)));
-    ui->textBrowser->setPalette(pl);
 }
 
 AboutAuthor::~AboutAuthor()
